151fcb1f027d48396cd041272a0df502980fd899
Yordan <ayvazov.dani@gmail.com>
2026-06-15 21:09:43 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 
 def main():
@@ -6,19 +7,27 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
+
         if command == 'exit':
             break
+
         elif command.startswith("echo"):
             print(command.replace("echo ", ""))
+
         elif command.startswith("type"):
             command = command.replace('type ','')
+
             if command.startswith('echo') or command.startswith('exit'):
                 print(f'{command} is a shell builtin')
+
             else:
+
                 if os.path.exists(command) and os.access(path, os.X_OK):
                     print(os.path.abspath(command))
+
                 else:
                     print(f'{command}: not found')
+
         else:
             print(f'{command}: command not found')
 
