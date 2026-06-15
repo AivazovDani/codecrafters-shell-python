1ad5c6f34975eb02bfedf1a341935d6fe11fec14
Yordan <ayvazov.dani@gmail.com>
2026-06-15 20:25:30 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,9 +2,12 @@ import sys
 
 
 def main():
+    # REPL (read the command, parse and evaluate (execute) it, display the output, return to step 1)
     while True:
         sys.stdout.write("$ ")
         command = input()
+        if command == 'exit':
+            break
         print(f'{command}: command not found')
 
 
