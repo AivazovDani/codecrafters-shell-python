4a12e2f18f41136d8265f03dff2bae1abff71221
Yordan <ayvazov.dani@gmail.com>
2026-06-17 16:51:56 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -11,28 +11,33 @@ builtins = ['echo', 'exit', 'type', 'cd', 'pwd']
 def completer(text, state): # built in eadline but overriding it to fit my case
             options = []
             # check builtins
-            options += [cmd + ' ' for cmd in builtins if cmd.startswith(text)]
-            
-            # here the state means how many times we pressed the tab. Each time we press the tab we cycle throught the commands in our options. Readlines update the state every time like: tab 1 = state=0 ; tab 2 = state=1. So the state it becomes index we can use to get the options in our list
-            
-            if len(options) == 0: # if we don\t have any PATH executable in our list with commands
-                # checking for executables in each directory in PATH (directories in linux where executable programs are stored)
-                for directory in os.environ.get('PATH', '').split(":"):
-                    if os.path.exists(directory):
-                        options += [f + ' ' for f in os.listdir(directory) if f.startswith(text)] # checking for all the commands that match our stdin
+
+            if ' ' not in command: # if we have space means the command is typed and we're waiting for args
+                options += [cmd + ' ' for cmd in builtins if cmd.startswith(text)]
+
+                # here the state means how many times we pressed the tab. Each time we press the tab we cycle throught the commands in our options. Readlines update the state every time like: tab 1 = state=0 ; tab 2 = state=1. So the state it becomes index we can use to get the options in our list
                 
-                options = sorted(options) # sorting alfabetically | worst case O(log n)
+                if len(options) == 0: # if we don\t have any PATH executable in our list with commands
+                    # checking for executables in each directory in PATH (directories in linux where executable programs are stored)
+
+                    for directory in os.environ.get('PATH', '').split(":"):
+                        if os.path.exists(directory):
+                            options += [f + ' ' for f in os.listdir(directory) if f.startswith(text)] # checking for all the commands that match our stdin
+                        
+                    options = sorted(options) # sorting alfabetically | worst case O(log n)
+            
+            else:
+                options = [cmd for cmd in os.listdir('.') if cmd.startswith(text)]
 
 
             if len(options) == 1 and state == 0: # if there is only 1 executable match
                 return options[0]
-                
+
                 return None # tell readline there are no more commands
 
             return options[state] if state < len(options) else None
 
 
-            
 def display_matches(substitution, options, longest_match_len): # this is build in readline but i override it to fit my case
     print()
     print("  ".join(sorted(options)))
@@ -149,5 +154,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
