60b839351f5fed21e23a8adfd5450b6741d0a9e3
Yordan <ayvazov.dani@gmail.com>
2026-06-26 14:37:13 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,92 +10,113 @@ import re
 # Programmic Completions
 completers = {}
 
-# Handeling TAB completion for build in commands and PATH executables | HARD
+# Handeling TAB completion for build in commands and PATH executables
 builtins = ['echo', 'exit', 'type', 'cd', 'pwd', 'complete', 'jobs', 'history', 'declare']
 
-# Background Jobs List
+# Background Jobs List | Jobs
 jobs = []
-written_commands = 0 # we declare it once at the start / refresh of the shell
 
-# Declaire Variables
+
+# We declare it once at the start / refresh of the shell | History
+written_commands = 0
+
+# Declaire Variables | Declare
 declares = {}
 
-def completer(text, state): # built in eadline but overriding it to fit my case
 
-            line = readline.get_line_buffer() # get's the command the user typed from the buffer
-            words = line.split() # split it into parts to determine if there are 2 or less. If less the user is still typing a command not an args
-            options = []
-            # check builtins
 
-            # checks for command then space and searches in completers for the command and suggest commands
-            if ' ' in line and len(words) >= 1 and words[0] in completers:
-                completer_path = completers[words[0]] # get the path for the command in completers
-                
-                env = os.environ.copy() # copy of all the enviroment variables as a dict into env
-                env['COMP_LINE'] = line # set a new key, value
-                env['COMP_POINT'] = str(len(line))
-                
-                
-                result = subprocess.run(
-                    [completer_path, words[0], text, words[-2] if len(words) > 2 else words[0]], # by default subprocess accept 3 args
-                    capture_output=True,
-                    text=True,
-                    env=env
-                )
-                options = [line + ' ' for line in result.stdout.splitlines()]
-                # "push\npull\n".splitlines() > ["push", "pull"]
-                # ["push " , "pull "] after adding trailing space
-                return options[state] if options else None
+class AutoCompleter:
+    def __init__(self, completers, builtins):
+        self.completers = completers
+        self.builtins = builtins
+
+    def completer(self, text, state):
+
+        options = []
+
+        line = readline.get_line_buffer() # user command from buffer
+        words = line.split() # split it into parts
+
+        # If user is typing args | Check 1
+
+        if ' ' in line and len(words) >= 1 and words[0] in self.completers:
             
+            completer_path = self.completers[words[0]] # get the path for the command from completers dict
+            
+            env = os.environ.copy() # copy of all the enviroment variables (as a dict) into env
+            
+            env['COMP_LINE'] = line # set a new key, value
+            
+            env['COMP_POINT'] = str(len(line)) # same
+            
+            
+            result = subprocess.run(
+                [completer_path, words[0], text, words[-2] if len(words) > 2 else words[0]], # by default subprocess accepts 3 args
+                capture_output=True,
+                text=True,
+                env=env
+            )
 
+            options = [match + ' ' for match in result.stdout.splitlines()]
 
-            if len(words) <= 1: # if there is no space means the user has typed a command not args to a command for autocomplete
-                # readline.get_line_buffer() return the full current line the user has typed, cause the command variable is set in our main function and this is outside it
-                options += [cmd + ' ' for cmd in builtins if cmd.startswith(text)]
+            # "push\npull\n".splitlines() => ["push", "pull"]
+            # ["push " , "pull "] after adding trailing space
 
-                # here the state means how many times we pressed the tab. Each time we press the tab we cycle throught the commands in our options. Readlines update the state every time like: tab 1 = state=0 ; tab 2 = state=1. So the state it becomes index we can use to get the options in our list
-                
-                if len(options) == 0: # if we don\t have any PATH executable in our list with commands
-                    # checking for executables in each directory in PATH (directories in linux where executable programs are stored)
+            return options[state] if options else None
+        
+        # User is typing command | Check 2
 
-                    for directory in os.environ.get('PATH', '').split(":"):
-                        if os.path.exists(directory):
-                            options += [f + ' ' for f in os.listdir(directory) if f.startswith(text)] # checking for all the commands that match our stdin
+        elif len(words) <= 1: # if there is no space == user has typed/typing a command
+            
+            options += [cmd + ' ' for cmd in self.builtins if cmd.startswith(text)]
+            
+            if len(options) == 0: # if the command is not built in, we need to check if it's a PATH executable
+
+                for directory in os.environ.get('PATH', '').split(":"):
+
+                    if os.path.exists(directory):
                         
-                    options = sorted(options) # sorting alfabetically | worst case O(log n)
+                        options += [f + ' ' for f in os.listdir(directory) if f.startswith(text)] # checking for all the PATH executables that match our stdin
+                    
+                options = sorted(options) # sorting alfabetically | worst case O(log n)
+        
+        else: # args not in completers dict
+
+            word = line.split()[-1] # get the args only
             
+            parts = word.rsplit("/", 1) # split them by /
+            
+            if len(parts) > 1 and os.path.isdir(parts[0]):
+                path = parts[0]
+                prefix = parts[1]
+
             else:
-                word = readline.get_line_buffer().split()[-1] # get the args only
-                parts = word.rsplit("/", 1) # split them by /
-                
-                if len(parts) > 1 and os.path.isdir(parts[0]):
-                    path = parts[0]
-                    prefix = parts[1]
-                else:
-                    path = "."
-                    prefix = word
-    
-                options = [f + ' ' for f in os.listdir(path) if f.startswith(prefix)]
+
+                path = "."
+                prefix = word
+
+            options = [f + ' ' for f in os.listdir(path) if f.startswith(prefix)]
 
 
-            if len(options) == 1 and state == 0: # if there is only 1 executable match
-                return options[0]
+        if len(options) == 1 and state == 0: # if there is only 1 executable match
+            return options[0] if state == 0 else None
 
-                return None # tell readline there are no more commands
+            
 
-            return options[state] if state < len(options) else None
+        return options[state] if state < len(options) else None
 
+    def display_matches(self, substitution, options, longest_match_len): # this is build in readline but i override it to fit my case
+        sys.stdout.write("\n")
+        sys.stdout.write("  ".join(options))
+        sys.stdout.write("\n$ " + readline.get_line_buffer())
+        sys.stdout.flush()
 
-def display_matches(substitution, options, longest_match_len): # this is build in readline but i override it to fit my case
-    sys.stdout.write("\n")
-    sys.stdout.write("  ".join(options))
-    sys.stdout.write("\n$ " + readline.get_line_buffer())
-    sys.stdout.flush()
+    def setup(self):
+        readline.set_completer(self.completer) # register your tab completion function
+        readline.parse_and_bind("tab: complete") # bind tab key to completion
+        readline.set_completion_display_matches_hook(self.display_matches) # runs when there are more than one match to show
+        readline.set_auto_history(True) # It tells readline to automatically add every command typed with input() to the history.
 
-readline.set_completer(completer) # register your tab completion function
-readline.parse_and_bind("tab: complete") # bind tab key to completion
-readline.set_completion_display_matches_hook(display_matches) # runs when there are more than one match to show
-readline.set_auto_history(True) # It tells readline to automatically add every command typed with input() to the history.
 
 def run_builtins(command):
     global written_commands # remembers every built in command from the current session
@@ -278,59 +299,173 @@ def run_builtins(command):
                     return f'declare -- {parts[2]}="{declares[parts[2]]}"'
         
 
-
-                
-
-                
-def main():
+def setup_history():
     # Sets the history file path 
-    HISTFILE = os.environ.get("HISTFILE") or os.path.expanduser("~/.shell_history") # if HISTFILE doesn't exist set it to home directory/.shell_history
+
+    histfile = os.environ.get("HISTFILE") or os.path.expanduser("~/.shell_history")
 
     try:
-        readline.read_history_file(HISTFILE) # tries to load previous sessions history commands so i can use the arrow up key
+
+        readline.read_history_file(histfile) # tries to load previous sessions history commands so i can use the arrow up key
+        
     except (OSError, FileNotFoundError):
-        pass  # file doesn't exist yet that's fine
+        pass # file doesn't exist yet that's fine
+
+    return histfile
+
+
+
+# Check for running jobs before the shell starts
+def check_background_jobs():
+    output = ""
+
+    for i in range(len(jobs)):
+        if jobs[i] == None:
+            continue
+        
+        process, original = jobs[i]
+
+        if process.poll() is not None:
+            not_none_indicess = [i for i in range(len(jobs)) if jobs[i] is not None]
+            
+            if i == not_none_indicess[-1] or len(not_none_indicess) == 1: # highest job number
+                marker = '+'
+            elif i == not_none_indicess[-2]: # second highest job number
+                marker = '-'
+            else:
+                marker = ' '
+
+            output += f'[{i+1}]{marker}  {'Done':<24}{original}' + '\n'
+            jobs[i] = None
+    
+    return output
+
+
+def replace_variable_name(command):
+    # Replace variable name with the real value
+    for name, value in declares.items():
+        command = command.replace(f'${{{name}}}', value)
+        command = command.replace(f'${name}', value)
+
+    command = re.sub(r'\$\{?\w+\}?', '', command)  # remove any remaining $vars
+
+
+    return command
+
+
+def pipelines(command):
+    parts = command.split('|')
+
+    first_command = parts[0]
+
+    if first_command.split()[0] in builtins:
+        result = run_builtins(first_command)
+        
+        
+        if result is not None:
+            result = result.encode() + b'\n'
+
+            for i in range(1, len(parts)):
+
+                if (len(parts) - 1) == i:
+                    if parts[i].split()[0] not in builtins:
+                        p = subprocess.Popen(shlex.split(parts[i].strip()), stdin=subprocess.PIPE)
+                        p.communicate(input=result)
+                    else:
+                        command = parts[i].strip()
+                        print(run_builtins(command))
+
+                
+                else:
+                    if isinstance(result, bytes):
+                        p = subprocess.Popen(shlex.split(parts[i].strip()), stdin=subprocess.PIPE)
+                        p.communicate(input=result)
+                    else:
+                        result = subprocess.Popen(shlex.split(parts[i].strip()), stdin=result.stdout, stdout=subprocess.PIPE)
+
+
+    else:
+
+        for i in range(len(parts)):
+
+            if i == 0:
+                result = subprocess.Popen(shlex.split(parts[i].strip()), stdout=subprocess.PIPE)
+            
+            elif (len(parts) - 1) == i:
+                if parts[i].split()[0] not in builtins:
+                    result = subprocess.Popen(shlex.split(parts[i].strip()), stdin=result.stdout)
+                    result.communicate()
+                else:
+                    command = parts[i].strip()
+                    print(run_builtins(command))
+
+            
+            else:
+                result = subprocess.Popen(shlex.split(parts[i].strip()), stdin=result.stdout, stdout=subprocess.PIPE)
+
+
+
+def run_tasks_in_background(command):
+    command = command.rstrip('&').strip()
+    parts = shlex.split(command)
+    cmd = parts[0]
+    args = parts[1:]
+    
+    path = shutil.which(cmd) # finds full path of executable in PATH and checks for executable permissions
+    if path:
+        process = subprocess.Popen([path] + args) # starts a process and returns immediately without waiting for it to finish:
+        
+        slot_found = False
+        for i in range(len(jobs)):
+
+            if jobs[i] == None:
+                slot_found = True
+                break
+        
+        if slot_found:
+            jobs[i] = (process, command)
+        else:
+            jobs.append((process, command))
+
+        return f'[{len(jobs)}] {process.pid}'
+    else:
+        return f'{cmd}: command not found'
+
+
+
+def main():
+
+
+    # History Commands
+    HISTFILE = setup_history()
+
+    # Auto Complete
+    completer = AutoCompleter(builtins, completers)
+    completer.setup()
 
     # REPL (read the command, parse and evaluate (execute) it, display the output, return to step 1)
     while True:
         
-        # Check for running jobs before the shell starts
-        for i in range(len(jobs)):
-            if jobs[i] == None:
-                continue
-            
-            process, original = jobs[i]
-
-            if process.poll() is not None:
-                not_none_indicess = [i for i in range(len(jobs)) if jobs[i] is not None]
-                
-                if i == not_none_indicess[-1] or len(not_none_indicess) == 1: # highest job number
-                    marker = '+'
-                elif i == not_none_indicess[-2]: # second highest job number
-                    marker = '-'
-                else:
-                    marker = ' '
-
-                print(f'[{i+1}]{marker}  {'Done':<24}{original}')
-                jobs[i] = None
+        # Check for background jobs
+        result = check_background_jobs()
+        if result:
+            print(result, end='')
 
         command = input("$ ")
 
-        # Replace variable name with the real value
-        for name, value in declares.items():
-            command = command.replace(f'${{{name}}}', value)
-            command = command.replace(f'${name}', value)
-            
-        command = re.sub(r'\$\{?\w+\}?', '', command)  # remove any remaining $vars
+        # Replace variables $NAME, {} in command
+        command = replace_variable_name(command)
 
 
         if command == 'exit':
             
             result = run_builtins(command)
+            
             if result == 'exit':
                 break
 
         elif '2>>' in command:
+            
             parts = command.split("2>>")
 
             file = parts[1]
@@ -341,6 +476,7 @@ def main():
                 subprocess.run(command_parts, stderr=f)
 
         elif '>>' in command or '1>>' in command:
+
             command = command.replace("1>>", ">>")
             parts = command.split(">>")
 
@@ -376,56 +512,7 @@ def main():
 
         # Pipelines
         elif '|' in command:
-            parts = command.split('|')
-
-            first_command = parts[0]
-
-            if first_command.split()[0] in builtins:
-                result = run_builtins(first_command)
-                
-                
-                if result is not None:
-                    result = result.encode() + b'\n'
-
-                    for i in range(1, len(parts)):
-
-                        if (len(parts) - 1) == i:
-                            if parts[i].split()[0] not in builtins:
-                                p = subprocess.Popen(shlex.split(parts[i].strip()), stdin=subprocess.PIPE)
-                                p.communicate(input=result)
-                            else:
-                                command = parts[i].strip()
-                                print(run_builtins(command))
-
-                        
-                        else:
-                            if isinstance(result, bytes):
-                                p = subprocess.Popen(shlex.split(parts[i].strip()), stdin=subprocess.PIPE)
-                                p.communicate(input=result)
-                            else:
-                                result = subprocess.Popen(shlex.split(parts[i].strip()), stdin=result.stdout, stdout=subprocess.PIPE)
-
-
-            else:
-
-                for i in range(len(parts)):
-
-                    if i == 0:
-                        result = subprocess.Popen(shlex.split(parts[i].strip()), stdout=subprocess.PIPE)
-                    
-                    elif (len(parts) - 1) == i:
-                        if parts[i].split()[0] not in builtins:
-                            result = subprocess.Popen(shlex.split(parts[i].strip()), stdin=result.stdout)
-                            result.communicate()
-                        else:
-                            command = parts[i].strip()
-                            print(run_builtins(command))
-
-                    
-                    else:
-                        result = subprocess.Popen(shlex.split(parts[i].strip()), stdin=result.stdout, stdout=subprocess.PIPE)
-
-
+            pipelines(command)
 
 
         elif command.startswith('pwd'):
@@ -443,7 +530,6 @@ def main():
         elif command.startswith("type"):
             result = run_builtins(command)
             print(result)
-
 
         elif command.startswith('complete'):
             result = run_builtins(command)
@@ -463,32 +549,8 @@ def main():
 
         # Run tasks in the background
         elif command.endswith('&'):
-            command = command.rstrip('&').strip()
-            parts = shlex.split(command)
-            cmd = parts[0]
-            args = parts[1:]
-            
-            path = shutil.which(cmd) # finds full path of executable in PATH and checks for executable permissions
-            if path:
-                process = subprocess.Popen([path] + args) # starts a process and returns immediately without waiting for it to finish:
-                
-                slot_found = False
-                for i in range(len(jobs)):
-
-                    if jobs[i] == None:
-                        slot_found = True
-                        break
-                
-                if slot_found:
-                    jobs[i] = (process, command)
-                else:
-                    jobs.append((process, command))
-
-                print(f'[{len(jobs)}] {process.pid}')
-            else:
-                print(f'{cmd}: command not found')
-
-                
+            result = run_tasks_in_background(command)
+            print(result)
 
         else:
             parts = shlex.split(command)
@@ -507,4 +569,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
